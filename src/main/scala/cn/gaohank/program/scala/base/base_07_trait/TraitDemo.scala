6136014792f3df44a789a32ddc3447aa49213935
gaosongling <gaosongling@xiaomi.com>
2018-11-20 08:59:04 +0800

    增加了traitDemo

---
+++ src/main/scala/cn/gaohank/program/scala/base/base_07_trait/TraitDemo.scala
@@ -0,0 +1,53 @@
+package cn.gaohank.program.scala.base.base_07_trait
+
+
+
+trait Listen{
+    val name: String
+    def listen() = {
+        println("You friend " + name + " is listening")
+    }
+}
+
+trait Read{
+    val name: String
+    def read() = {
+        println("You friend " + name + " is reading")
+    }
+}
+
+trait Speak{
+    val name: String
+    def speak() = {
+        println("You friend " + name + " is speaking.")
+    }
+}
+
+class Human(val name : String){
+    def listen() = {
+        println(name + " is listening.")
+    }
+}
+
+class Animal(val name: String){
+
+}
+
+class Cat( val name: String)  extends Speak with Listen with Read{
+    override def toString: String = getClass.getName + " hello "+name+"! "
+}
+
+object TraitDemo {
+    def main(args: Array[String]) {
+//        val f1 = new Listen();//java 接口很像 不能直接够造
+//        val h1 = new Human("gaga")
+//        h1.listen()
+
+        //这样scat就是Friend了
+        val scat = new Cat("hello kitty")
+        scat.listen()
+        scat.speak()
+        scat.read()
+        println(scat)
+    }
+}
