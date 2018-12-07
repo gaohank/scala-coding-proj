54fc0e14ef3248a324eb08d80af30c2058b2b00a
gaosongling <gaosongling@xiaomi.com>
2018-12-07 17:59:18 +0800

    修改list

--- src/main/scala/cn/gaohank/program/scala/base/base_04_collection/ListDemo.scala
+++ src/main/scala/cn/gaohank/program/scala/base/base_04_collection/ListDemo.scala
@@ -14,6 +14,7 @@ import scala.collection.mutable.ListBuffer
 object ListDemo {
     def main(args: Array[String]): Unit = {
         val list = List(1, 2, 3)
+        println(list(1))
         // List(0, 1, 2, 3)
         println(0 :: list)
         // List(0, 1, 2, 3)
